296a1fe1b7f602a7c89130aac6f6e5c9f1dc01ce
cht-coder <105980463+cht-coder@users.noreply.github.com>
2023-12-17 15:11:09 +0530

    Fixes

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,11 +7,7 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
-import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import { Carousel } from "react-bootstrap";
-
-import "./styles/home.css";
 
 const CARD_DATAS = [
   {
@@ -37,49 +33,22 @@ const CARD_DATAS = [
 const Home = () => {
   return (
     <>
-      <div className="home-container">
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://placekitten.com/1200/400"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>MARRIAGE EVENT PLANNER</h3>
-              <p>HEARTY WELCOME</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://placekitten.com/1200/401"
-              alt="Second slide"
-            />
-            <Carousel.Caption>
-              <h3>JOURNEY TO JOYFUL UNION</h3>
-              <p>BE A HAPPY COUPLE</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          {/* Add more Carousel.Items for additional slides */}
-        </Carousel>
-        <Stack
-          sx={{
-            flexDirection: "row",
-            my: 5,
-            gap: "2.5rem",
-            justifyContent: "center",
-            "& .MuiButton-root": { width: "12.5rem" },
-          }}
-        >
-          <Button variant="contained" color="success" href="/login">
-            Login
-          </Button>
-          <Button variant="contained" color="success" href="/register">
-            Register
-          </Button>
-        </Stack>
-      </div>
+      <Stack
+        sx={{
+          flexDirection: "row",
+          my: 5,
+          gap: "2.5rem",
+          justifyContent: "center",
+          "& .MuiButton-root": { width: "12.5rem" },
+        }}
+      >
+        <Button variant="contained" color="success" href="/login">
+          Login
+        </Button>
+        <Button variant="contained" color="success" href="/register">
+          Register
+        </Button>
+      </Stack>
       <CardsGrid />
     </>
   );
